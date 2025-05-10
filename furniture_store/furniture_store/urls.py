9e099defc28125bb2c8d865f000fd7bca4fc6831
Roman Beskokotov <roma@gmail.com>
2025-05-10 17:01:42 +0300

    default settings

--- furniture_store/furniture_store/urls.py
+++ furniture_store/furniture_store/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Index.as_view(), name='index'),
+    path('about/', views.About.as_view(), name='about'),
 ]
--- furniture_store/main/views.py
+++ furniture_store/main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views import View
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+class Index(TemplateView):
+    template_name = 'main/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        context['content'] = 'Главная страцниа магазина Home'
+        return context
+    
+
+class About(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('hello word')
