22cd46fe4ff008375fcfcc03ec97a2bd4d9fa166
Roman Beskokotov <roma@gmail.com>
2025-05-10 17:13:53 +0300

    add gitignore

--- furniture_store/main/views.py
+++ furniture_store/main/views.py
@@ -8,6 +8,7 @@ from django.http import HttpRequest, HttpResponse
 class Index(TemplateView):
     template_name = 'main/index.html'
     
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home'
